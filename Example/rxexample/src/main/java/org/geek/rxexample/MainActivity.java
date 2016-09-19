83cb2e9444b59b42b338c8618887cac04af35e1f
zhanggeng <zhanggengdyx@163.com>
2016-09-19 15:39:52 +0800

    add ButterKnife test

--- Example/rxexample/src/main/java/org/geek/rxexample/MainActivity.java
+++ Example/rxexample/src/main/java/org/geek/rxexample/MainActivity.java
@@ -1,13 +1,42 @@
 package org.geek.rxexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello)
+    TextView textView;
+
+    Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unbinder = ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello)
+    public void onclickTv() {
+        Toast.makeText(this, "tv hello click", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
     }
 }
